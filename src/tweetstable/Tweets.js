484966bdef815679c7f955319fc2208ba9b05c85
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 09:37:20 +0200

    Add tweets component.

---
+++ src/tweetstable/Tweets.js
@@ -0,0 +1,77 @@
+import {Component} from "react";
+import Joi from "joi-browser";
+import TweetsTable from "./TweetsTable";
+import SearchBoxForm from "../components/SearchBox";
+import Pagination from "../components/Pagination";
+import {getPageData} from "../helperfunction/GetPageData";
+import {getTweets} from "../service/TweetsService";
+
+export default class Tweets extends Component {
+    state = {
+        tweets: [],
+        searchQuery: "",
+        pageSize: 5,
+        currentPage: 1,
+        sortColumn: {path: "name", order: 'asc'},
+        data: {
+            name: "",
+            tweet: "",
+            date: ""
+        },
+        errors: {}
+    };
+
+    schema = {
+        name: Joi.string().label("Writer"),
+        tweet: Joi.string().label("Tweet")
+    };
+
+    async componentDidMount() {
+        const {data: tweets} = await getTweets();
+        this.setState({tweets});
+    };
+
+    render() {
+        const {tweets, searchQuery, currentPage, pageSize, sortColumn} = this.state;
+        const {length, dataPaginate} = getPageData(tweets, pageSize, currentPage, sortColumn, searchQuery, 0);
+        return (
+            <div>
+                <SearchBoxForm
+                    value={searchQuery}
+                    onChange={this.handleSearch}
+                />
+                <TweetsTable
+                    tweets={dataPaginate}
+                    onComment={this.handleComment}
+                    onSort={this.handleSort}
+                    sortColumn={sortColumn}
+                />
+
+                <Pagination
+                    pageSize={pageSize}
+                    length={length}
+                    currentPage={currentPage}
+                    onPageChange={this.handlePageChange}
+                />
+            </div>
+        )
+    }
+
+    handlePageChange = page => this.setState({currentPage: page});
+
+    handleSearch = query => this.setState({searchQuery: query, currentPage: 1});
+
+    handleSort = path => {
+        const sortColumn = {...this.state.sortColumn};
+        if (sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+
+        this.setState({sortColumn});
+    };
+
+    handleComment = tweet => {
+        const {tweets} = this.state;
+        const tweetsFilter = tweets.filter(t => t._id === tweet._id);
+    }
+    
+}
