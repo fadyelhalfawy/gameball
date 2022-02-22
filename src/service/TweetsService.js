47de3be181c55e1642496b973f1408167f9ee9ce
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 09:41:39 +0200

    Add httpservice using API.

--- src/service/TweetsService.js
+++ src/service/TweetsService.js
@@ -1,4 +1,9 @@
 import http from "./HttpService";
 import config from "../config/config.json";
 
-export const getClients = () => http.get(config.clientsPath);
+export const getTweets = () => http.get(config.tweetsPath);
+
+// export function addTweet(tweet) {
+//     return http.post(config.tweetsPath,{
+//
+//     })};
