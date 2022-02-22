7cc0f0eba44b5817b0accd67c34ab2a60abcf20f
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 09:37:49 +0200

    Add searchbox component.

---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+const SearchBoxForm = ( { value, onChange} ) => {
+    return(
+        <input
+            type={"text"}
+            name={"query"}
+            className={"form-control my-3"}
+            placeholder={"Search..."}
+            value={value}
+            onChange={e => onChange(e.currentTarget.value)}
+        />
+    );
+}
+
+export default SearchBoxForm;
