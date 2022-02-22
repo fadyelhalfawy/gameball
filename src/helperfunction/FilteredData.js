177f248a08a7f3b417da5109bdf5d017d2b22324
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 09:38:53 +0200

    Add filteredData function.

--- src/helperfunction/FilteredData.js
+++ src/helperfunction/FilteredData.js
@@ -1,9 +1,9 @@
 export let filteredData = (allData, searchQuery, number) => {
     let data = allData;
 
-    if (searchQuery && number === 0) data = allData.filter(d => d.name.toLowerCase().startsWith(searchQuery.toLowerCase()));
+    if (searchQuery && number === 0) data = allData.filter(d => d.user.name.toLowerCase().startsWith(searchQuery.toLowerCase()));
 
-    else if (searchQuery && number === 1) data = allData.filter(d => d.sender.toLowerCase().startsWith(searchQuery.toLowerCase()));
+    // else if (searchQuery && number === 1) data = allData.filter(d => d.sender.toLowerCase().startsWith(searchQuery.toLowerCase()));
 
     return data;
 };
