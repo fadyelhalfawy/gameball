763c2e86924f6ccdcab2d8b28840f4bc92444ed9
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 19:47:52 +0200

    Add Reply component.

--- src/routers/Replys.js
+++ src/routers/Replys.js
@@ -1,5 +1,68 @@
-export const Replies = () => {
-    return(
-        <h1>All Replies!</h1>
-    );
+import {Component} from "react";
+import SearchBoxForm from "../components/SearchBox";
+import Pagination from "../components/Pagination";
+import {getPageData} from "../helperfunction/GetPageData";
+import RepliesTable from "../repliesTable/RepliesTable";
+import {getReplies} from "../service/RepliesService";
+
+export default class Replies extends Component {
+    state = {
+        replies: [],
+        searchQuery: "",
+        pageSize: 5,
+        currentPage: 1,
+        sortColumn: {path: "replier", order: 'asc'},
+        data: {
+            reply: "",
+            userReplier: "",
+            tweet: "",
+            comment: "",
+            date: ""
+        },
+        errors: {}
+    };
+
+    async componentDidMount() {
+        const {data: replies} = await getReplies();
+        this.setState({replies});
+    };
+
+    render() {
+        const {replies, searchQuery, currentPage, pageSize, sortColumn} = this.state;
+        const {length, dataPaginate} = getPageData(replies, pageSize, currentPage, sortColumn, searchQuery, 2);
+        return (
+            <div>
+                <SearchBoxForm
+                    value={searchQuery}
+                    onChange={this.handleSearch}
+                />
+                <RepliesTable
+                    replies={dataPaginate}
+                    onReply={this.handleReply}
+                    onSort={this.handleSort}
+                    sortColumn={sortColumn}
+                />
+
+                <Pagination
+                    pageSize={pageSize}
+                    length={length}
+                    currentPage={currentPage}
+                    onPageChange={this.handlePageChange}
+                />
+            </div>
+        )
+    }
+
+    handlePageChange = page => this.setState({currentPage: page});
+
+    handleSearch = query => this.setState({searchQuery: query, currentPage: 1});
+
+    handleSort = path => {
+        const sortColumn = {...this.state.sortColumn};
+        if (sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+
+        this.setState({sortColumn});
+    };
+
 }
