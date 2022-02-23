1d86c81bb6f6be269d1d1003befd6fb56daf333b
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 03:19:04 +0200

    Add InputForm component.

---
+++ src/forms/InputForm.js
@@ -0,0 +1,20 @@
+const InputForm = ({ name, label, error, ...rest }) => {
+    return(
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <input className="form-control clickable"
+                   {...rest}
+                   id={name}
+                   name={name}
+            />
+            {error && <div
+                className="alert alert-danger"
+                role="alert">
+                {error}
+            </div>
+            }
+        </div>
+    );
+}
+
+export default InputForm;
