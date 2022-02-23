7b309d2b8f8d2d2e461ea0322f043bcb9b89432c
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 21:25:37 +0200

    Update GetPageData function.

--- src/helperfunction/GetPageData.js
+++ src/helperfunction/GetPageData.js
@@ -3,11 +3,11 @@ import {Paginate} from "./Paginate";
 import {filteredData} from "./FilteredData";
 
 export const getPageData = (data, pageSize, currentPage, sortColumn, searchQuery, number) => {
-    const dataFiltered = filteredData(data, searchQuery, number);
+    const getData = filteredData(data, searchQuery, number);
 
-    const allData = _.orderBy(dataFiltered, [sortColumn.path], [sortColumn.order]);
+    const allData = _.orderBy(getData, [sortColumn.path], [sortColumn.order]);
 
     const dataPaginate = Paginate(allData, pageSize, currentPage);
 
-    return ({ length: dataFiltered.length, dataPaginate });
+    return ({ length: data.length, dataPaginate });
 }
