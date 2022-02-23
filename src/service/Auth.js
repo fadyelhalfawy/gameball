f4ca0ea0e69dfe1a1ff7baf8def2c9f45e022452
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 09:58:35 +0200

    Add AuthService.

---
+++ src/service/Auth.js
@@ -0,0 +1,38 @@
+import http from "./HttpService";
+import config from "../config/config.json";
+import jwtDecode from "jwt-decode";
+
+const tokenKey = "token";
+
+http.setJwt(getJwt());
+
+async function login(name) {
+    const { data: jwt } = await http.post(config.authPath, { name });
+    localStorage.setItem(tokenKey, jwt);
+}
+
+function getCurrentUser() {
+    try {
+        const jwt = localStorage.getItem(tokenKey);
+        return jwtDecode(jwt);
+    }
+    catch (e) {
+        return null;
+    }
+}
+
+function getJwt() {
+    return localStorage.getItem(tokenKey);
+}
+
+function logout() {
+    localStorage.removeItem(tokenKey);
+}
+
+const auth = {
+    login,
+    getCurrentUser,
+    logout
+}
+
+export default auth;
