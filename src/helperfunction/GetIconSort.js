9ccd5275ed910f63e7de6ec9271e3dc67ec45dbd
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 04:26:50 +0200

    Update getIconSort.

--- src/helperfunction/GetIconSort.js
+++ src/helperfunction/GetIconSort.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaSortAlphaDown, FaSortAlphaUpAlt } from "react-icons/fa";
 
 export const getIconSort = (column, sortColumn) => {
     if(column.path !== sortColumn.path) return null;
 
-    if(sortColumn.order === 'asc') return <i className={"fa fa-sort-asc"}></i>
+    if(sortColumn.order === 'asc') return <FaSortAlphaDown />
 
-    return <i className={"fa fa-sort-desc"}></i>
+    return <FaSortAlphaUpAlt />
 }
