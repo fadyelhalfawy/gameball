dd1fa1cd86c6a7591b613691c7fcf211472260d3
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 21:25:24 +0200

    Update FilteredData function.

--- src/helperfunction/FilteredData.js
+++ src/helperfunction/FilteredData.js
@@ -1,13 +1,12 @@
 export let filteredData = (allData, searchQuery, number) => {
     let data = allData;
+    console.log(data.user);
 
     if (searchQuery && number === 0) data = allData.filter(d => d.user.name.toLowerCase().startsWith(searchQuery.toLowerCase()));
 
     else if (searchQuery && number === 1) data = allData.filter(d => d.comment.toLowerCase().startsWith(searchQuery.toLowerCase()));
 
-    else if (searchQuery && number === 2) data = allData.filter(d => d.comment.toLowerCase().startsWith(searchQuery.toLowerCase()));
-
-    else return;
+    else if (searchQuery && number === 2) data = allData.filter(d => d.reply.toLowerCase().startsWith(searchQuery.toLowerCase()));
 
     return data;
 };
