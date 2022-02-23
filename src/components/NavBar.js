7944b9da34ad38e849a7e564c02b37dbde38e2d1
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-23 21:56:40 +0200

    Delete unused lines.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,9 +9,6 @@ const NavBar = ({ user }) => {
             <Link className="navbar-brand container" to="/">Twitter...</Link>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <NavLink className="nav-item nav-link" to="/home">
-                        Home
-                    </NavLink>
                     <NavLink className="nav-item nav-link" to="/tweets">
                         Tweets
                     </NavLink>
@@ -20,9 +17,6 @@ const NavBar = ({ user }) => {
                     </NavLink>
                     <NavLink className="nav-item nav-link" to="/replies">
                         Replies
-                    </NavLink>
-                    <NavLink className="nav-item nav-link" to="/about-us">
-                        About Us
                     </NavLink>
                 </div>
 
